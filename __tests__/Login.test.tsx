7cf986fcada3e632718882c675eb9e6aa59fc88b
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-01-15 14:34:47 -0300

    Agregado: createMockRouter y test de Login

---
+++ __tests__/Login.test.tsx
@@ -0,0 +1,79 @@
+import Home from "@/app/page";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import { createMockRouter } from "../__mocks__/createMockRouter";
+
+describe("LOGIN PAGE", () => {
+  beforeEach(() => {
+    render(<Home />);
+  });
+
+  it("Deberia tener el nombre de la app como titulo", () => {
+    expect(
+      screen.getByRole("heading", {
+        name: /smart pocket/i,
+      })
+    ).toBe(/smart pocket/i);
+  });
+
+  it("Deberia tener un h2 descriptivo de la app", () => {
+    expect(
+      screen.getByRole("heading", {
+        name: /controla tus gastos de manera sencilla y ahorra para tus proximos objetivos/i,
+      })
+    ).toBeInTheDocument();
+  });
+
+  it("Deberia tener el formulario de iniciar sesion", () => {
+    expect(screen.getByText(/email/i)).toBeInTheDocument();
+    expect(screen.getByText(/contraseña/i)).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: /iniciar sesion/i })
+    ).toBeInTheDocument();
+  });
+
+  it("Deberia tener los links de redireccion a recuperar contraseña", async () => {
+    const router = createMockRouter({
+      pathname: "/",
+    });
+
+    const recover_password = screen.getByRole("link", {
+      name: /olvidaste tu contraseña\?/i,
+    });
+
+    expect(recover_password).toBeInTheDocument();
+
+    await userEvent.click(recover_password);
+    const pushed = router.push;
+
+    expect(pushed).toHaveBeenCalledWith(
+      "/recover_password",
+      "/recover_password"
+    );
+  });
+
+  it("Deberia tener los links de redireccion a register", async () => {
+    const router = createMockRouter({
+      pathname: "/",
+    });
+
+    const register = screen.getByRole("link", {
+      name: /no tenes una cuenta\? registrate\!/i,
+    });
+
+    expect(register).toBeInTheDocument();
+
+    await userEvent.click(register);
+    const pushed = router.push;
+
+    expect(pushed).toHaveBeenCalledWith("/register", "/register");
+  });
+
+  it("Deberia tener las opciones de login con google y facebook", () => {
+    expect(
+      screen.getByRole("button", { name: /facebook/i })
+    ).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /google/i })).toBeInTheDocument();
+  });
+});
