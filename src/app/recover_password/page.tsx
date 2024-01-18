75cbebc6f0e7a7ee9267a718dbcf97714c7e164d
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-01-18 10:38:49 -0300

    Agregado: postEnvio de email con loader

--- src/app/recover_password/page.tsx
+++ src/app/recover_password/page.tsx
@@ -1,10 +1,20 @@
-import React, { useId } from "react";
+"use client";
+import React, { useId, useState } from "react";
 import styles from "@/app/recover_password/page.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
 const Recover_password = () => {
+  const [emailSended, setEmailSended] = useState<boolean>(false);
+  const [load, setLoad] = useState(false);
   const $emailId = useId();
+
+  const sendEmail = () => {
+    setEmailSended(true);
+    setTimeout(() => {
+      setLoad(true);
+    }, 1500);
+  };
 
   return (
     <div className={styles.Recover_password}>
@@ -26,11 +36,23 @@ const Recover_password = () => {
           costraseña.
         </h3>
       </div>
-      <div className={styles.Recover_passwordForm}>
-        <label htmlFor={$emailId}>Email</label>
-        <input type="email" id={$emailId} required />
-        <button>Recuperar contraseña</button>
-      </div>
+      {emailSended ? (
+        <>
+          {load ? (
+            <div className={styles.Recover_passwordSended}>
+              <p>Revisa tu email. Ya enviamos las instrucciones.</p>
+            </div>
+          ) : (
+            <div className={styles.Recover_passwordLoader}></div>
+          )}
+        </>
+      ) : (
+        <div className={styles.Recover_passwordForm}>
+          <label htmlFor={$emailId}>Email</label>
+          <input type="email" id={$emailId} required />
+          <button onClick={() => sendEmail()}>Recuperar contraseña</button>
+        </div>
+      )}
       <Link href={"/"}>Volver a iniciar sesion!</Link>
     </div>
   );
