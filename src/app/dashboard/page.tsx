ac6bda723d482ed6c126e03a2a0f31c88ef34eff
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-01-18 15:31:07 -0300

    Creado: SideBar Navigation

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,13 @@
+import SideBar from "@/components/navigation/SideBar";
 import React from "react";
+import styles from '@/app/dashboard/page.module.css';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className={styles.Dashboard}>
+      <SideBar />
+    </div>
+  );
 };
 
 export default Dashboard;
---
+++ src/components/navigation/SideBar.tsx
@@ -0,0 +1,87 @@
+"use client";
+import React from "react";
+import styles from "@/components/navigation/page.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const SideBar = () => {
+  const pathname = usePathname();
+
+  return (
+    <nav className={styles.SideBar}>
+      <ul className={styles.SideBar_List}>
+        <li className={styles.SideBar_LogoContainer}>
+          <Image
+            className={styles.SideBar_Logo}
+            src={"/img/smartpocket_black.svg"}
+            alt="money"
+            width={81}
+            height={81}
+          />
+        </li>
+        <li
+          className={
+            pathname === "/dashboard"
+              ? styles.SideBar_ListItemSelected
+              : styles.SideBar_ListItem
+          }
+        >
+          <Link className={styles.SideBar_Link} href={"/dashboard"}>
+            <Image
+              src={"/img/dashboard.svg"}
+              alt="dashboard"
+              width={51}
+              height={51}
+            />
+          </Link>
+        </li>
+        <li
+          className={
+            pathname === "/dashboard/expense"
+              ? styles.SideBar_ListItemSelected
+              : styles.SideBar_ListItem
+          }
+        >
+          <Link className={styles.SideBar_Link} href={"/dashboard/expense"}>
+            <Image
+              src={"/img/expense.svg"}
+              alt="expense"
+              width={51}
+              height={51}
+            />
+          </Link>
+        </li>
+        <li
+          className={
+            pathname === "/dashboard/category"
+              ? styles.SideBar_ListItemSelected
+              : styles.SideBar_ListItem
+          }
+        >
+          <Link className={styles.SideBar_Link} href={"/dashboard/category"}>
+            <Image
+              src={"/img/category.svg"}
+              alt="category"
+              width={51}
+              height={51}
+            />
+          </Link>
+        </li>
+        <li
+          className={
+            pathname === "/dashboard/chart"
+              ? styles.SideBar_ListItemSelected
+              : styles.SideBar_ListItem
+          }
+        >
+          <Link className={styles.SideBar_Link} href={"/dashboard/chart"}>
+            <Image src={"/img/chart.svg"} alt="chart" width={51} height={51} />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default SideBar;
