331777a778efcd39f63bc5eba0e4c238efa54fc5
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-01-18 15:39:26 -0300

    Agregado: Layout para dashboard y sus rutas

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Titillium_Web } from "next/font/google";
+import "../globals.css";
+import SideBar from "@/components/navigation/SideBar";
+
+const titi = Titillium_Web({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "600", "700", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Smart Pocket",
+  description: "APP de control de gastos personales.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <body className={titi.className + " dashboardBody"}>
+        <SideBar />
+        {children}
+      </body>
+    </html>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,10 @@
-import SideBar from "@/components/navigation/SideBar";
 import React from "react";
 import styles from '@/app/dashboard/page.module.css';
 
 const Dashboard = () => {
   return (
     <div className={styles.Dashboard}>
-      <SideBar />
+      
     </div>
   );
 };
