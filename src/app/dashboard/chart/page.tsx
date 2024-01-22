bbe54cb6f08a898215dab125fb8a2a196fae59c2
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-01-22 11:13:59 -0300

    Agregado: hojas css en category chart y expense

--- src/app/dashboard/chart/page.tsx
+++ src/app/dashboard/chart/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import styles from "./page.module.css";
 
 const Chart = () => {
-  return <div>Chart</div>;
+  return <div className={styles.Chart}>Chart</div>;
 };
 
 export default Chart;
--- src/app/dashboard/expense/page.tsx
+++ src/app/dashboard/expense/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import styles from "./page.module.css";
 
 const Expense = () => {
-  return <div>Expense</div>;
+  return <div className={styles.Expense}>Expense</div>;
 };
 
 export default Expense;
