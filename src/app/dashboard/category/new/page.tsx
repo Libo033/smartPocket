73051f2bcd50bb096541654facbcf396f39f7e94
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-01-23 14:16:01 -0300

    Refactor: CateogryForm ahora es componente

--- src/app/dashboard/category/new/page.tsx
+++ src/app/dashboard/category/new/page.tsx
@@ -1,45 +1,16 @@
 "use client";
-import React, { FormEvent, useId, useState } from "react";
+import React, { useState } from "react";
 import styles from "../page.module.css";
 import Link from "next/link";
-import { CirclePicker, ColorResult, HuePicker } from "react-color";
+import { ColorResult } from "react-color";
 import { useRouter } from "next/navigation";
+import CategoryForm from "@/components/category/CategoryForm";
 
 const CategoryNew = () => {
-  const router = useRouter();
   const [background, setBackground] = useState<string>("#ffffff");
-  const $name = useId();
-  const $color = useId();
-
-  const colors = [
-    "#f44336",
-    "#e91e63",
-    "#9c27b0",
-    "#673ab7",
-    "#3f51b5",
-    "#2196f3",
-    "#03a9f4",
-    "#00bcd4",
-    "#009688",
-    "#4caf50",
-    "#8bc34a",
-    "#cddc39",
-    "#ffeb3b",
-    "#ffc107",
-    "#ff9800",
-    "#ff5722",
-    "#795548",
-    "#607d8b",
-  ];
 
   const handleColors = (color: ColorResult) => {
     setBackground(color.hex + "45");
-  };
-
-  const createCategory = (e: FormEvent) => {
-    e.preventDefault();
-
-    router.push("/dashboard/category");
   };
 
   return (
@@ -52,36 +23,7 @@ const CategoryNew = () => {
           Volver
         </Link>
       </div>
-      <form
-        className={styles.Category_Form}
-        onSubmit={(Event: FormEvent) => createCategory(Event)}
-        action="post"
-      >
-        <div className={styles.Category_FormContainer}>
-          <label htmlFor={$name}>Nombre</label>
-          <input type="text" id={$name} autoComplete="off" required />
-        </div>
-        <div className={styles.Category_FormContainer}>
-          <label htmlFor={$color}>Color</label>
-          <HuePicker
-            className={styles.Category_FormColorPicker2}
-            color={background}
-            onChange={(color: ColorResult) => handleColors(color)}
-          />
-          <CirclePicker
-            className={styles.Category_FormColorPicker}
-            onChange={(color: ColorResult) => handleColors(color)}
-            circleSpacing={21}
-            circleSize={45}
-            colors={colors}
-          />
-        </div>
-        <div className={styles.Category_FormButtonContainer}>
-          <button type="submit" className={styles.Category_FormButton}>
-            Crear
-          </button>
-        </div>
-      </form>
+      <CategoryForm handleColors={handleColors} background={background} />
     </div>
   );
 };
---
+++ src/components/category/CategoryForm.tsx
@@ -0,0 +1,81 @@
+import React, { FormEvent, useId } from "react";
+import styles from "./page.module.css";
+import { CirclePicker, ColorResult, HuePicker } from "react-color";
+import {
+  ReadonlyURLSearchParams,
+  useRouter,
+  useSearchParams,
+} from "next/navigation";
+
+const CategoryForm: React.FC<{
+  handleColors: Function;
+  background: string;
+}> = ({ handleColors, background }) => {
+  const searchParams: ReadonlyURLSearchParams = useSearchParams();
+  const id: string | null = searchParams.get("id");
+  const router = useRouter();
+  const $name = useId();
+  const $color = useId();
+
+  const colors = [
+    "#f44336",
+    "#e91e63",
+    "#9c27b0",
+    "#673ab7",
+    "#3f51b5",
+    "#2196f3",
+    "#03a9f4",
+    "#00bcd4",
+    "#009688",
+    "#4caf50",
+    "#8bc34a",
+    "#cddc39",
+    "#ffeb3b",
+    "#ffc107",
+    "#ff9800",
+    "#ff5722",
+    "#795548",
+    "#607d8b",
+  ];
+
+  const handleCategory = (e: FormEvent) => {
+    e.preventDefault();
+
+    router.push("/dashboard/category");
+  };
+
+  return (
+    <form
+      className={styles.CategoryForm}
+      onSubmit={(Event: FormEvent) => handleCategory(Event)}
+      action="post"
+    >
+      <div className={styles.CategoryForm_Container}>
+        <label htmlFor={$name}>Nombre</label>
+        <input type="text" id={$name} autoComplete="off" required />
+      </div>
+      <div className={styles.CategoryForm_Container}>
+        <label htmlFor={$color}>Color</label>
+        <HuePicker
+          className={styles.CategoryForm_ColorPicker2}
+          color={background}
+          onChange={(color: ColorResult) => handleColors(color)}
+        />
+        <CirclePicker
+          className={styles.CategoryForm_ColorPicker}
+          onChange={(color: ColorResult) => handleColors(color)}
+          circleSpacing={21}
+          circleSize={45}
+          colors={colors}
+        />
+      </div>
+      <div className={styles.CategoryForm_ButtonContainer}>
+        <button type="submit" className={styles.CategoryForm_Button}>
+          Crear
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default CategoryForm;
