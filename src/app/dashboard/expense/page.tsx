ec83af448bf01849d0c198dc66b29840950dbb1a
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-01-25 14:35:30 -0300

    Modificacion: Nuevas rutas para API Income y movementes page

--- src/app/dashboard/expense/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./page.module.css";
-
-const Expense = () => {
-  return <div className={styles.Expense}>Expense</div>;
-};
-
-export default Expense;
