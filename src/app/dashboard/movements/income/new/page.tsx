bd0db901d914c0f154fe2c23607602635c154384
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-01-26 14:48:00 -0300

    En proceso: income pages id new

--- src/app/dashboard/movements/income/new/page.tsx
+++ src/app/dashboard/movements/income/new/page.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import styles from "../page.module.css";
+import Link from "next/link";
 
 const IncomeNew = () => {
-  return <div>IncomeNew</div>;
+  return (
+    <div className={styles.Income}>
+      <div className={styles.Income_TitleContainer}>
+        <h1 className={styles.Income_Title}>Nuevo Ingreso</h1>
+      </div>
+      <div className={styles.Income_LinkContainer}>
+        <Link className="linkA" href={"/dashboard/movements"}>
+          Volver
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default IncomeNew;
