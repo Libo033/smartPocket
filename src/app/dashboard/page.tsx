d145598f198f0e67b1f726c0bbe8f6f11111115e
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-01-29 16:38:07 -0300

    En proceso: iniciado dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import styles from '@/app/dashboard/page.module.css';
+import styles from "@/app/dashboard/page.module.css";
 
 const Dashboard = () => {
   return (
     <div className={styles.Dashboard}>
-      
+      <div className={styles.Dashboard_TitleContainer}>
+        <h1 className={styles.Dashboard_Title}>Tablero</h1>
+      </div>
     </div>
   );
 };
