556f3d3e4bd70124b95cb33356b05b6dde60dd31
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-01-29 15:47:37 -0300

    Creado e implementado: ExpenseForm componente

--- src/components/expense/ExpenseForm.tsx
+++ src/components/expense/ExpenseForm.tsx
@@ -1,17 +1,17 @@
 "use client";
-import React, { FormEvent, useContext, useId, useState } from "react";
+import React, {
+  FormEvent,
+  useContext,
+  useEffect,
+  useId,
+  useState,
+} from "react";
 import styles from "./page.module.css";
 import { useRouter } from "next/navigation";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 import { MovementContext } from "@/context/MovementsContext";
 import { CategoryContext } from "@/context/CategoryContext";
-import {
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  SelectChangeEvent,
-} from "@mui/material";
+import { IExpense } from "@/libs/interfaces";
 
 const ExpenseForm: React.FC<{ id: string | null }> = ({ id }) => {
   const router: AppRouterInstance = useRouter();
@@ -28,15 +28,64 @@ const ExpenseForm: React.FC<{ id: string | null }> = ({ id }) => {
     e.preventDefault();
 
     if (id) {
+      const date: string = (document.getElementById($date) as HTMLInputElement)
+        .value;
+      let y: number = parseInt(date.slice(0, 4));
+      let m: number = parseInt(date.slice(5, 7));
+      let d: number = parseInt(date.slice(8, 10));
+
+      const expenseToEdit: IExpense = {
+        _id: id,
+        category_id: category,
+        expense,
+        day: d,
+        month: m - 1,
+        year: y,
+      };
+
+      if (expense > 0) {
+        editExpense(expenseToEdit);
+      }
     } else {
+      const date: string = (document.getElementById($date) as HTMLInputElement)
+        .value;
+      let y: number = parseInt(date.slice(0, 4));
+      let m: number = parseInt(date.slice(5, 7));
+      let d: number = parseInt(date.slice(8, 10));
+
+      if (expense > 0) {
+        createExpense(category, expense, d, m - 1, y);
+      }
     }
 
     router.push("/dashboard/movements");
   };
 
   const handleDeleteExpense = (id: string) => {
+    deleteExpense(id);
     router.push("/dashboard/movements");
   };
+
+  useEffect(() => {
+    if ((document.getElementById($date) as HTMLInputElement) && id === null) {
+      (document.getElementById($date) as HTMLInputElement).valueAsDate =
+        new Date();
+    }
+    if (id) {
+      expenses.forEach((e) => {
+        if (id === e._id) {
+          setExpense(e.expense);
+          setCategory(e.category_id);
+          (document.getElementById($date) as HTMLInputElement).value =
+            e.year +
+            "-" +
+            (e.month < 10 ? "0" + (e.month + 1) : e.month + 1) +
+            "-" +
+            (e.day < 10 ? "0" + e.day : e.day);
+        }
+      });
+    }
+  }, []);
 
   return (
     <form
